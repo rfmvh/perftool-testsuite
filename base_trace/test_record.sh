22e5fe0300fb7b0a559fddd4b17007316ed088b9
Michael Petlan <mpetlan@redhat.com>
2015-10-29 17:02:24 +0100

    base_trace: trim forgotten whitespace

--- base_trace/test_record.sh
+++ base_trace/test_record.sh
@@ -31,7 +31,7 @@ $CMD_PERF report --stdio > record_basic_report.log 2> record_basic_report.err
 CHECK_EXIT_CODE=$?
 # check the perf report output
 ../common/check_all_lines_matched.pl "$RE_LINE_REPORT_CONTENT" "$RE_LINE_EMPTY" "$RE_LINE_COMMENT" < record_basic_report.log
-(( CHECK_EXIT_CODE += $? )) 
+(( CHECK_EXIT_CODE += $? ))
 # check that the perf report stderr is empty
 test ! -s record_basic_report.err
 (( CHECK_EXIT_CODE += $? ))
