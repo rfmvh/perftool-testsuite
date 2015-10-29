046b10e0f798b115401127ae6f8c7ee07c5dc47b
Michael Petlan <mpetlan@redhat.com>
2015-10-29 15:49:16 +0100

    base_stat: Added test for Intel uncore events
    In case there are any Intel uncore events available, this test uses
    them with perf-stat. Otherwise the test is skipped.
    
    The fact *whether* uncore events should be supported on a particular
    machine should be tested in base_list testcases once.
    
    Zero values are accepted as correct results too. This thing should
    be rethought once more. But nowadays it is pretty usual, that an
    uncore event is shown in perf-list's output, but when used it gives
    "<not supported> string. This test should catch such situations.

--- base_stat/cleanup.sh
+++ base_stat/cleanup.sh
@@ -16,5 +16,6 @@ find . -name \*.log | xargs rm
 test -d hw && rmdir hw
 test -d hwcache && rmdir hwcache
 test -d hv24x7 && rmdir hv_24x7
+test -d intel_uncore && rmdir intel_uncore
 print_overall_results 0
 exit 0
---
+++ base_stat/test_intel_uncore.sh
@@ -0,0 +1,46 @@
+#
+#	test_intel_uncore of perf_stat test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		This test tests support of uncore events
+#
+#
+
+# include working environment
+. ../common/settings.sh
+. ../common/patterns.sh
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0`
+TEST_RESULT=0
+
+EVENTS_TO_TEST=`$CMD_PERF list | grep "uncore" | awk '{print $1}' | tr '\n' ' '`
+if [ -z "$EVENTS_TO_TEST" ]; then
+	print_testcase_skipped
+	exit 0
+fi
+
+test -d intel_uncore || mkdir intel_uncore
+
+
+#### testing Intel uncore events
+
+for event in $EVENTS_TO_TEST; do
+	EVENT_NAME=`echo $event | tr '/' '_' | tr ',' '-'`
+	$CMD_PERF stat -a -e $event -o intel_uncore/$EVENT_NAME.log -x';' -- $CMD_QUICK_SLEEP
+	PERF_EXIT_CODE=$?
+
+	REGEX_LINES="$RE_NUMBER;[^;]*;$RE_EVENT_ANY;$RE_NUMBER;100\.00"
+	../common/check_all_patterns_found.pl "$REGEX_LINES" < intel_uncore/$EVENT_NAME.log
+	CHECK_EXIT_CODE=$?
+	print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "event $event"
+	(( TEST_RESULT += $? ))
+done
+
+
+# print overall resutls
+print_overall_results "$TEST_RESULT"
+exit $?
