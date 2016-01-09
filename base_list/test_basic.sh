2345dfecb4ece2243a9ce54e05522be53421a0e2
Michael Petlan <mpetlan@redhat.com>
2016-01-09 17:57:06 +0100

    base_list: Move logs and temporary files outside the tree

--- base_list/test_basic.sh
+++ base_list/test_basic.sh
@@ -22,12 +22,12 @@ TEST_RESULT=0
 
 if [ "$PARAM_GENERAL_HELP_TEXT_CHECK" = "y" ]; then
 	# test that a help message is shown and looks reasonable
-	$CMD_PERF list --help > basic_helpmsg.log
+	$CMD_PERF list --help > $LOGS_DIR/basic_helpmsg.log
 	PERF_EXIT_CODE=$?
 
-	../common/check_all_patterns_found.pl "PERF-LIST" "NAME" "SYNOPSIS" "DESCRIPTION" "EVENT MODIFIERS" "RAW HARDWARE" "PARAMETERIZED EVENTS" "OPTIONS" "SEE ALSO" "NOTES" < basic_helpmsg.log
+	../common/check_all_patterns_found.pl "PERF-LIST" "NAME" "SYNOPSIS" "DESCRIPTION" "EVENT MODIFIERS" "RAW HARDWARE" "PARAMETERIZED EVENTS" "OPTIONS" "SEE ALSO" "NOTES" < $LOGS_DIR/basic_helpmsg.log
 	CHECK_EXIT_CODE=$?
-	../common/check_all_patterns_found.pl "perf\-list \- List all symbolic event types" < basic_helpmsg.log
+	../common/check_all_patterns_found.pl "perf\-list \- List all symbolic event types" < $LOGS_DIR/basic_helpmsg.log
 	(( CHECK_EXIT_CODE += $? ))
 
 	print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "help message"
@@ -40,7 +40,7 @@ fi
 ### basic execution
 
 # test that perf list is even working
-$CMD_PERF list > basic_basic.log 2> basic_basic.err
+$CMD_PERF list > $LOGS_DIR/basic_basic.log 2> $LOGS_DIR/basic_basic.err
 PERF_EXIT_CODE=$?
 
 REGEX_LINE_HEADER="List of pre-defined events"
@@ -48,9 +48,9 @@ REGEX_LINE_BASIC="\s*$RE_EVENT_ANY\s+(?:OR\s+$RE_EVENT_ANY\s+)?\[.*event.*\]"
 REGEX_LINE_BREAKPOINT="\s*mem:<addr>.*\s+\[Hardware breakpoint\]"
 REGEX_LINE_RAW="\[Raw hardware event descriptor\]"
 REGEX_LINE_AUX="see \'man perf\-list\' on how to encode it"
-../common/check_all_lines_matched.pl "$RE_LINE_EMPTY" "$REGEX_LINE_HEADER" "$REGEX_LINE_BASIC" "$REGEX_LINE_BREAKPOINT" "$REGEX_LINE_RAW" "$REGEX_LINE_AUX" < basic_basic.log
+../common/check_all_lines_matched.pl "$RE_LINE_EMPTY" "$REGEX_LINE_HEADER" "$REGEX_LINE_BASIC" "$REGEX_LINE_BREAKPOINT" "$REGEX_LINE_RAW" "$REGEX_LINE_AUX" < $LOGS_DIR/basic_basic.log
 CHECK_EXIT_CODE=$?
-test ! -s basic_basic.err
+test ! -s $LOGS_DIR/basic_basic.err
 (( CHECK_EXIT_CODE += $? ))
 
 print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "basic execution"
@@ -72,10 +72,10 @@ outputs["syscalls:\*"]="^\s*syscalls:sys"
 
 for i in ${!outputs[@]}; do
 	j=`echo $i | tr -d '\\\*:'`
-	$CMD_PERF list $i > basic_$j.log
+	$CMD_PERF list $i > $LOGS_DIR/basic_$j.log
 	PERF_EXIT_CODE=$?
 
-	../common/check_all_lines_matched.pl "$REGEX_LINE_HEADER" "$RE_LINE_EMPTY" "${outputs[$i]}" < basic_$j.log
+	../common/check_all_lines_matched.pl "$REGEX_LINE_HEADER" "$RE_LINE_EMPTY" "${outputs[$i]}" < $LOGS_DIR/basic_$j.log
 	CHECK_EXIT_CODE=$?
 
 	print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "list $i"
@@ -86,10 +86,10 @@ done
 ### non-sense argument
 
 # 'perf list somethingnonsense' caused segfault in 4.4-rc
-$CMD_PERF list somethingnonsense &> basic_nonsense.log
+$CMD_PERF list somethingnonsense &> $LOGS_DIR/basic_nonsense.log
 PERF_EXIT_CODE=$?
 
-../common/check_no_patterns_found.pl "SIGSEGV" "egmentation fault" < basic_nonsense.log
+../common/check_no_patterns_found.pl "SIGSEGV" "egmentation fault" < $LOGS_DIR/basic_nonsense.log
 CHECK_EXIT_CODE=$?
 
 print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "non-sense argument"
