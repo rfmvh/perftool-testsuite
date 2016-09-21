37c97768ae5b82680f2d748dde8179056ec607e1
Michael Petlan <mpetlan@redhat.com>
2016-09-21 16:10:43 +0200

    base_list/test_powerpc_hv_24x7.sh: new testcase
    This test tests correct listing of hv_24x7 events and their names.
    These events are available on POWER8 LPAR machine only. On other
    arches/machines it should be skipped.
    
    A correct name is the following:
    
    hv_24x7/HPM_SYNC_PCYC,domain=?,core=?/
    
    An incorrect one is this:
    
    hv_24x7/HPM_TC_2_CCYC__PHYS_CORE,core=?/
    
    The domain should be specified as an argument, not being a part of
    the name. The domain numbers are the following:
    
    $ cat /sys/bus/event_source/devices/hv_24x7/interface/domains
    1: Physical Chip
    2: Physical Core
    3: VCPU Home Core
    4: VCPU Home Chip
    5: VCPU Home Node
    6: VCPU Remote Node

---
+++ base_list/test_powerpc_hv_24x7.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#
+#	test_powerpc_hv24x7 of perf_list test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		This test tests correct showing of hv_24x7 events.
+#	These events are expected to be supported on POWER8 LPAR
+#	machines only.
+#
+#		The 24x7 events used to have the domain they belong
+#	to specified in their names, which is not currently true,
+#	domain is specified as an argument now. Perf list should
+#	show corresponding information.
+#
+
+# include working environment
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0 .sh`
+TEST_RESULT=0
+
+# ppc64, ppc64le (or ppc64el) only
+if ! [[ "$MY_ARCH" =~ ppc64.* ]]; then
+	print_overall_skipped
+	exit 0
+fi
+
+# POWER8 only
+LD_SHOW_AUXV=1 /bin/true | grep -q -i POWER8
+if [ $? -ne 0 ]; then
+	print_overall_skipped
+	exit 0
+fi
+
+# detect virtualization
+type virt-what 2>/dev/null >/dev/null
+if [ $? -ne 0 ]; then
+	VIRT="unknown"
+else
+	VIRT=`virt-what`
+fi
+
+# we can continue only if VIRT is 'LPAR' or unknown
+echo "$VIRT" | grep -q -i -e "lpar" -e "unknown"
+if ! [ "$VIRT" = "" -o "$VIRT" = "unknown" ]; then
+	print_overall_skipped
+	exit 0
+fi
+
+
+### listing hv_24x7 events
+
+# test that perf list is even working
+$CMD_PERF list hv_24x7 > $LOGS_DIR/hv_24x7_list.log
+PERF_EXIT_CODE=$?
+
+REGEX_HV_ARG=",(?:domain|core)=\?"
+REGEX_HV_EVENTNAME="[A-Za-z0-9](?:[A-Za-z0-9]|_[A-Za-z0-9])+"
+REGEX_HV_EVENT="hv_24x7\/$REGEX_HV_EVENTNAME(?:$REGEX_HV_ARG)+\/"
+REGEX_LINE_BASIC="^\s*$REGEX_HV_EVENT\s+\[Kernel\sPMU\sevent\]"
+../common/check_all_lines_matched.pl "$REGEX_LINE_BASIC" < $LOGS_DIR/hv_24x7_list.log
+CHECK_EXIT_CODE=$?
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "listing events"
+(( TEST_RESULT += $? ))
+
+
+# print overall results
+print_overall_results "$TEST_RESULT"
+exit $?
