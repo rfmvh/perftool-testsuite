b71b62a15efca0846046d0be56ba10a0e82d46da
Michael Petlan <mpetlan@redhat.com>
2016-10-20 14:48:19 +0200

    base_buildid/setup: log the sample recording more precisely

--- base_buildid/setup_src.sh
+++ base_buildid/setup_src.sh
@@ -23,7 +23,12 @@ clear_buildid_cache
 
 # record some perf.data
 $CMD_PERF --buildid-dir $BUILDIDDIR record -o $CURRENT_TEST_DIR/perf.data -a -- $CMD_LONGER_SLEEP &> $LOGS_DIR/setup.log
-print_results $? 0 "creating perf.data and buildid-cache"
+PERF_EXIT_CODE=$?
+
+../common/check_all_patterns_found.pl "$RE_LINE_RECORD1" "$RE_LINE_RECORD2" < $LOGS_DIR/setup.log
+CHECK_EXIT_CODE=$?
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "creating perf.data and buildid-cache"
 
 print_overall_results $?
 (( TEST_RESULT += $? ))
