92873b115133c05f53daa0145d0ff076c84c0e16
Saruman <Saruman@Rodrigo>
2016-11-17 23:08:49 +0100

    base_record/test_evlist.sh: introduced new testcase for perf-evlist
    The perf-evlist is a helper that shows all the events contained in
    perf.data (all the events of which there are samples). It should
    list events, be able to print the sampling frequencies or dump the
    whole event configuration for each of them.
    
    Currently covered:
    
      * evlisting frequencies (100, 200 and 1000)
      * evlisting various events
      * help message
    
    To be done:
    
      * evlisting events from perf.data which contains multiple events
      * verbose mode check
    
    We use hw events here, because they are the most basic ones that
    are reliable to produce samples. If we sampled cpu-migrations, it
    might not produce anything (too few event occurrences, only one
    CPU, etc.) and that would make the results unstable.
    
    However, since not all machines support hw sampling, the test can
    be skipped, if there is nothing to measure. This is not great, so
    I will add some events of other kinds that are reliable and should
    be available everywhere... in future...

---
+++ base_record/test_evlist.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+#
+#	test_evlist of perf_record test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		This test tests basic functionality of perf evlist command,
+#	which should show events used in a sampling session saved in perf.data
+#
+
+# include working environment
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0 .sh`
+TEST_RESULT=0
+
+
+### help message
+
+if [ "$PARAM_GENERAL_HELP_TEXT_CHECK" = "y" ]; then
+	# test that a help message is shown and looks reasonable
+	$CMD_PERF evlist --help > $LOGS_DIR/basic_helpmsg.log
+	PERF_EXIT_CODE=$?
+
+	../common/check_all_patterns_found.pl "PERF-EVLIST" "NAME" "SYNOPSIS" "DESCRIPTION" "OPTIONS" "SEE ALSO" < $LOGS_DIR/basic_helpmsg.log
+	CHECK_EXIT_CODE=$?
+	../common/check_all_patterns_found.pl "input" "verbose" "freq" "group" < $LOGS_DIR/basic_helpmsg.log
+	(( CHECK_EXIT_CODE += $? ))
+
+	print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "help message"
+	(( TEST_RESULT += $? ))
+else
+	print_testcase_skipped "help message"
+fi
+
+
+### sample frequency check
+
+EVENT="cpu-cycles"
+CYCLES_AVAIL=`$CMD_PERF list | grep "$EVENT"`
+if [ -n "$CYCLES_AVAIL" ]; then
+	for frq in 100 200 1000; do
+		# record with frequency $frq
+		$CMD_PERF record -a -e $EVENT -o $CURRENT_TEST_DIR/perf.data -F $frq -- $CMD_LONGER_SLEEP > $LOGS_DIR/evlist_freq_record_$frq.log 2> $LOGS_DIR/evlist_freq_record_$frq.err
+		PERF_EXIT_CODE=$?
+
+		../common/check_all_patterns_found.pl "$RE_LINE_RECORD1" "$RE_LINE_RECORD2" "perf.data" < $LOGS_DIR/evlist_freq_record_$frq.err
+		CHECK_EXIT_CODE=$?
+
+		print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "sample frequency check :: record with $frq"
+		(( TEST_RESULT += $? ))
+
+		# check $frq is in evlist output
+		$CMD_PERF evlist -i $CURRENT_TEST_DIR/perf.data -F > $LOGS_DIR/evlist_freq_evlist_$frq.log
+		PERF_EXIT_CODE=$?
+
+		../common/check_all_patterns_found.pl "$EVENT:\s+sample_freq=$frq" < $LOGS_DIR/evlist_freq_evlist_$frq.log
+		CHECK_EXIT_CODE=$?
+
+		print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "sample frequency check :: evlist $frq"
+		(( TEST_RESULT += $? ))
+	done
+else
+	print_testcase_skipped "sample frequency check"
+fi
+
+
+
+### TODO verbose evlisting
+
+
+### evlist with various events
+
+EVENTS_TO_TEST=`$CMD_PERF list hw | grep "Hardware event" | awk '{print $1}' | egrep '^.' | tr '\n' ' '`
+if [ -n "$EVENTS_TO_TEST" ]; then
+	for event in $EVENTS_TO_TEST; do
+		# record event
+		$CMD_PERF record -a -e $event -o $CURRENT_TEST_DIR/perf.data -- $CMD_LONGER_SLEEP > $LOGS_DIR/evlist_record_$event.log 2> $LOGS_DIR/evlist_record_$event.err
+		PERF_EXIT_CODE=$?
+
+		../common/check_all_patterns_found.pl "$RE_LINE_RECORD1" "$RE_LINE_RECORD2" "perf.data" < $LOGS_DIR/evlist_record_$event.err
+		CHECK_EXIT_CODE=$?
+
+		print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "various events :: record $event"
+		(( TEST_RESULT += $? ))
+
+		# evlist it
+		$CMD_PERF evlist -i $CURRENT_TEST_DIR/perf.data > $LOGS_DIR/evlist_evlist_$event.log
+		PERF_EXIT_CODE=$?
+
+		../common/check_all_patterns_found.pl "$event" < $LOGS_DIR/evlist_evlist_$event.log
+		CHECK_EXIT_CODE=$?
+		../common/check_all_lines_matched.pl "$event" < $LOGS_DIR/evlist_evlist_$event.log
+		(( CHECK_EXIT_CODE += $? ))
+
+		print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "various events :: evlist $event"
+		(( TEST_RESULT += $? ))
+	done
+else
+	print_testcase_skipped "various events"
+fi
+
+
+
+### TODO multiple events at once
+
+
+# print overall results
+print_overall_results "$TEST_RESULT"
+exit $?
