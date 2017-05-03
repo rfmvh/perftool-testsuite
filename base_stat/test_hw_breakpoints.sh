f2c75d4b885b26a8aca79ece0d21c1abff625b9b
Michael Petlan <mpetlan@redhat.com>
2017-05-03 17:24:50 +0200

    base_stat/test_hw_breakpoints.sh: added test for kernelspace readwrite
    This will test mem:<addr>:rw event on vm_dirty_ratio variable reading.

--- base_stat/test_hw_breakpoints.sh
+++ base_stat/test_hw_breakpoints.sh
@@ -53,6 +53,25 @@ print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "kspace address execution mem:$AD
 (( TEST_RESULT += $? ))
 
 
+### kernelspace address readwrite
+
+# perf stat -e mem:<addr>:rw
+TEST_VAR="vm_dirty_ratio"
+ADDR="0x`grep "$TEST_VAR" /proc/kallsyms | cut -f1 -d' '`"
+$CMD_PERF stat -e mem:$ADDR:rw -x';' -- sysctl vm.dirty_ratio > /dev/null 2> $LOGS_DIR/hw_breakpoints_k_addr_rw.err
+PERF_EXIT_CODE=$?
+
+# non-zero number of breakpoint hits is expected (around 3)
+REGEX_PERF_STAT_OUTPUT="^[1-9]\d*;;mem:$ADDR:rw;$RE_NUMBER;100.00;"
+../common/check_all_patterns_found.pl "$REGEX_PERF_STAT_OUTPUT" < $LOGS_DIR/hw_breakpoints_k_addr_rw.err
+CHECK_EXIT_CODE=$?
+../common/check_all_lines_matched.pl "$REGEX_PERF_STAT_OUTPUT" < $LOGS_DIR/hw_breakpoints_k_addr_rw.err
+(( CHECK_EXIT_CODE += $? ))
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "kspace address readwrite mem:$ADDR:rw"
+(( TEST_RESULT += $? ))
+
+
 # print overall results
 print_overall_results "$TEST_RESULT"
 exit $?
