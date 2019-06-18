a352f7c77af2991cb0c09b9fe21333bb1ad7b46e
Saruman <saruman@libertymail.net>
2019-06-18 16:26:25 +0200

    base_other: added two testcases for /proc/sys/kernel/perf_* sysctl settings
    Also covers the scenario fixed by the following linux commit:
    
    	commit 1a51c5da5acc6c188c917ba572eebac5f8793432
    	Author: Stephane Eranian <eranian@google.com>
    	Date:   Thu Jan 10 17:17:16 2019 -0800
    
    	perf core: Fix perf_proc_update_handler() bug
    
    When perf_cpu_time_max_percent is either 0 or 100, perf_event_max_sample_rate
    tweaking should be disabled. Previously, although kernel refused the changes,
    the variable gets changed anyway. After the fix, the change-blocking actually
    takes effect.

---
+++ base_other/test_perf_cpu_time_max_percent.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#
+#	test_perf_cpu_time_max_percent of other perf_tests
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		This test tests if perf_event_perf_cpu_time_max_percent can
+#	be set to values 0-100 only.
+#
+#
+
+# include working environment
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0 .sh`
+TEST_RESULT=0
+
+VARIABLE="/proc/sys/kernel/perf_cpu_time_max_percent"
+VARIABLE_BACKUP=`cat $VARIABLE`
+
+
+### correct values
+
+VALUES="0 1 10 25 51 99 100"
+EXP_RESULT=0  # PASS
+for val in $VALUES; do
+	echo $val > $VARIABLE 2> $LOGS_DIR/perf_cpu_time_max_percent_$val.err
+	test $? -eq $EXP_RESULT
+	WRITE_EXIT_CODE=$?
+
+	test `cat $VARIABLE` -eq $val
+	VERIFICATION_EXIT_CODE=$?
+
+	print_results $WRITE_EXIT_CODE $VERIFICATION_EXIT_CODE "correct values: $val"
+	(( TEST_RESULT += $? ))
+done
+
+
+### incorrect values
+
+VALUES="-1 101 255 256 1025"
+EXP_RESULT=1  # FAIL
+for val in $VALUES; do
+	echo $val > $VARIABLE 2> $LOGS_DIR/perf_cpu_time_max_percent_$val.err
+	test $? -eq $EXP_RESULT
+	WRITE_EXIT_CODE=$?
+
+	../common/check_all_lines_matched.pl "write error: Invalid argument" < $LOGS_DIR/perf_cpu_time_max_percent_$val.err
+	CHECK_EXIT_CODE=$?
+	../common/check_all_patterns_found.pl "write error: Invalid argument" < $LOGS_DIR/perf_cpu_time_max_percent_$val.err
+	(( CHECK_EXIT_CODE += $? ))
+
+	print_results $WRITE_EXIT_CODE $CHECK_EXIT_CODE "incorrect values: $val"
+	(( TEST_RESULT += $? ))
+done
+
+
+# restore original value
+echo $VARIABLE_BACKUP > $VARIABLE
+
+
+# print overall results
+print_overall_results "$TEST_RESULT"
+exit $?
---
+++ base_other/test_perf_event_max_sample_rate.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+#
+#	test_perf_event_max_sample_rate of other perf_tests
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		This test tests perf_event_max_sample_rate setting if perf_cpu_time_max_percent
+#	disables changing of it.
+#
+#
+
+# include working environment
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0 .sh`
+TEST_RESULT=0
+
+MAX_PERCENT="/proc/sys/kernel/perf_cpu_time_max_percent"
+MAX_PERCENT_BACKUP=`cat $MAX_PERCENT`
+MAX_PERCENT_SANE_VALUE=25
+
+MAX_SAMPLE_RATE="/proc/sys/kernel/perf_event_max_sample_rate"
+MAX_SAMPLE_RATE_BACKUP=`cat $MAX_SAMPLE_RATE`
+
+
+### changing enabled
+
+echo $MAX_PERCENT_SANE_VALUE > $MAX_PERCENT
+VALUES="28000 $MAX_SAMPLE_RATE_BACKUP"
+EXP_RESULT=0  # PASS
+for val in $VALUES; do
+	echo $val > $MAX_SAMPLE_RATE 2> $LOGS_DIR/perf_event_max_sample_rate_$MAX_PERCENT_SANE_VALUE.err
+	test $? -eq $EXP_RESULT
+	WRITE_EXIT_CODE=$?
+
+	test `cat $MAX_SAMPLE_RATE` -eq $val
+	CHECK_EXIT_CODE=$?
+
+	print_results $WRITE_EXIT_CODE $CHECK_EXIT_CODE "changes enabled (perf_cpu_time_max_percent = $MAX_PERCENT_SANE_VALUE): $val"
+	(( TEST_RESULT += $? ))
+done
+
+
+### changing disabled
+
+EXP_RESULT=1
+SOME_VALUE=$(( MAX_SAMPLE_RATE_BACKUP / 2 ))
+for val in 0 100; do
+	# set MAX_PERCENT to a value that prevents MAX_SAMPLE_RATE changes
+	echo $val > $MAX_PERCENT
+	WRITE_EXIT_CODE=$?
+
+	# the following attempt of changing MAX_SAMPLE_RATE should fail:
+	echo $SOME_VALUE > $MAX_SAMPLE_RATE 2> $LOGS_DIR/perf_event_max_sample_rate_$val.err
+	test $? -eq $EXP_RESULT
+	(( WRITE_EXIT_CODE += $? ))
+
+	# check that there was an error message
+	../common/check_all_lines_matched.pl "write error: Invalid argument" < $LOGS_DIR/perf_event_max_sample_rate_$val.err
+	CHECK_EXIT_CODE=$?
+	../common/check_all_patterns_found.pl "write error: Invalid argument" < $LOGS_DIR/perf_event_max_sample_rate_$val.err
+	(( CHECK_EXIT_CODE += $? ))
+
+	# we also need to check that the value of $MAX_SAMPLE_RATE did not change
+	test `cat $MAX_SAMPLE_RATE` -eq $MAX_SAMPLE_RATE_BACKUP
+	(( CHECK_EXIT_CODE += $? ))
+	test `cat $MAX_SAMPLE_RATE` -ne $SOME_VALUE
+	(( CHECK_EXIT_CODE += $? ))
+
+	print_results $WRITE_EXIT_CODE $CHECK_EXIT_CODE "changes disabled (perf_cpu_time_max_percent = $val): $SOME_VALUE"
+	(( TEST_RESULT += $? ))
+done
+
+
+# restore original values
+echo $MAX_PERCENT_SANE_VALUE > $MAX_PERCENT
+echo $MAX_SAMPLE_RATE_BACKUP > $MAX_SAMPLE_RATE
+
+# print overall results
+print_overall_results "$TEST_RESULT"
+exit $?
