8bc8e67305354c2e8149cdac8c1eb86ecf4dedd3
Michael Petlan <mpetlan@redhat.com>
2019-06-18 17:44:15 +0200

    base_stat/auxiliary/exec_perf.sh: event fix
    With cpu-clock, the test would be more universal and not dependent
    on hardware PMU.

--- base_stat/auxiliary/exec_perf.sh
+++ base_stat/auxiliary/exec_perf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 $CMD_DOUBLE_LONGER_SLEEP &
-exec $CMD_PERF stat -a -e cycles -I100 -- $CMD_LONGER_SLEEP 2> $LOGS_DIR/endless_wait.log
+exec $CMD_PERF stat -a -e cpu-clock -I100 -- $CMD_LONGER_SLEEP 2> $LOGS_DIR/endless_wait.log
