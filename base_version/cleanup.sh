6e5d1ff25aac51f2d1f91b186da737289b2565ad
Michael Petlan <mpetlan@redhat.com>
2019-07-23 18:07:59 +0200

    base_version: new test
    Yea, perf now has "version" subcommand too. The test is mainly supposed
    to check whether it works and whether it does not crash with an invalid
    commandline switch - as commit 916c31fff946fae0e05862f9b2435fdb29fd5090
    from upstream linux kernel fixes.

---
+++ base_version/cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+#	cleanup.sh of perf_version test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#		FIXME
+#
+#
+
+. ../common/init.sh
+. ./settings.sh
+
+if [ -n "$PERFSUITE_RUN_DIR" ]; then
+	print_overall_skipped
+	exit 0
+fi
+
+find . -name \*.out | xargs -r rm
+find . -name \*.err | xargs -r rm
+
+print_overall_results 0
+exit 0
---
+++ base_version/test_basic.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#
+#	test_basic of perf_version test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#
+#		This test tests basic functionality of perf version command.
+#
+#
+
+# include working environment
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0 .sh`
+TEST_RESULT=0
+
+
+#### basic execution
+
+# test that perf trace is working
+$CMD_PERF version > $LOGS_DIR/basic_basic.log
+PERF_EXIT_CODE=$?
+
+../common/check_all_lines_matched.pl "perf version" < $LOGS_DIR/basic_basic.log
+CHECK_EXIT_CODE=$?
+../common/check_all_patterns_found.pl "perf version" < $LOGS_DIR/basic_basic.log
+(( CHECK_EXIT_CODE += $? ))
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "basic execution"
+(( TEST_RESULT += $? ))
+
+
+#### invalid option
+
+# BUG: perf version crashes with an invalid option
+{ sh -c "$CMD_PERF version -t"; } > $LOGS_DIR/basic_invalid_opt.out 2> $LOGS_DIR/basic_invalid_opt.err
+test $? -ne 139
+PERF_EXIT_CODE=$?
+
+../common/check_no_patterns_found.pl "Segmentation fault" "SIGSEGV" "core" "dumped" "segfault" < $LOGS_DIR/basic_invalid_opt.err
+CHECK_EXIT_CODE=$?
+#../common/check_all_patterns_found.pl "Error" "unknown switch" "Usage" "perf version" "build-options" < $LOGS_DIR/basic_invalid_opt.err
+#(( CHECK_EXIT_CODE += $? ))
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "invalid option"
+(( TEST_RESULT += $? ))
+
+
+# print overall results
+print_overall_results "$TEST_RESULT"
+exit $?
