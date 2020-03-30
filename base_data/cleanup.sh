e4651a850096f6fc9c6dbaa72fcaa654fb9730de
Benjamin Salon <bsalon@redhat.com>
2020-03-30 17:19:06 +0200

    base_data/cleanup.sh: delete directories created by test
    In base_data/test_basic.sh, we are creating two new directories, which
    help us to test perf data command, so we should delete them afterwards.

--- base_data/cleanup.sh
+++ base_data/cleanup.sh
@@ -20,6 +20,8 @@ fi
 find . -name \*.log | xargs -r rm
 find . -name \*.err | xargs -r rm
 rm -f perf.data*
+rm -rf $LOGS_DIR/converted_ctf
+rm -rf $LOGS_DIR/converted_all_ctf
 
 print_overall_results 0
 exit 0
