28e265760b975eb49d38f2366d3d2605b62ce26f
Benjamin Salon <bsalon@redhat.com>
2020-05-22 15:13:10 +0200

    base_config/test_set_report.sh: test of some report variables
    This test tests three report variables. The report.sort_order test
    checks whether the variable changed sort order by comparing the output
    with default output. The report.percent-limit test tests if the new
    report.percent-limit value affects the percentage limit. It should not
    show percentages lower than the limit. The last test, report.children,
    checks if the percentage count after setting the variable is almost
    equal to 100. It also checks whether the percentage count is lower than
    before. This test uses the same approach to config file as
    'base_config/test_set.sh' test.

---
+++ base_config/test_set_report.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+#
+#       test_set_report of perf_config test
+#       Author: Benjamin Salon <bsalon@redhat.com>
+#
+#       Description:
+#
+#               This test tests functionality of setting report variables of perf config command.
+#
+#
+
+# include working environment
+. ../common/init.sh
+. ./settings.sh
+
+THIS_TEST_NAME=`basename $0 .sh`
+TEST_RESULT=0
+
+
+# save config file before tests
+touch $HOME/.perfconfig
+mv $HOME/.perfconfig $CURRENT_TEST_DIR/.config_before
+
+
+### record and report without config
+
+$CMD_PERF record -a -o $CURRENT_TEST_DIR/perf.data -- $CMD_SIMPLE 2> $LOGS_DIR/set_report_record.log
+PERF_EXIT_CODE=$?
+
+../common/check_all_patterns_found.pl "$RE_LINE_RECORD1" "$RE_LINE_RECORD2" "perf.data" < $LOGS_DIR/set_report_record.log
+CHECK_EXIT_CODE=$?
+
+$CMD_PERF report --stdio -i $CURRENT_TEST_DIR/perf.data > $LOGS_DIR/set_report_report_no_cfg.log 2> /dev/null
+(( PERF_EXIT_CODE += $? ))
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "general setup"
+(( TEST_RESULT += $? ))
+
+
+### report.sort_order variable
+
+# set the variable
+$CMD_PERF config --user report.sort_order=sym,dso
+PERF_EXIT_CODE=$?
+
+$CMD_PERF config --user --list > $LOGS_DIR/set_report_list.log
+(( PERF_EXIT_CODE += $? ))
+
+# check if the variable is set
+grep -q report.sort_order=sym,dso < $LOGS_DIR/set_report_list.log
+CHECK_EXIT_CODE=$?
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "setting report.sort_order variable"
+(( TEST_RESULT += $? ))
+
+
+# check if the variable changed sorting
+$CMD_PERF report --stdio -i $CURRENT_TEST_DIR/perf.data > $LOGS_DIR/set_report_sort.log 2> /dev/null
+PERF_EXIT_CODE=$?
+
+! cmp $LOGS_DIR/set_report_no_cfg.log $LOGS_DIR/set_report_sort.log 2> /dev/null
+CHECK_EXIT_CODE=$?
+
+REGEX_COMMENT_LINE="^#.*$"
+REGEX_DATA_LINE="\s*$RE_NUMBER%\s*\[[k\.]\]\s*[\w]+\s*[\[\]\.\w]+"
+
+# only data line is important for us
+../common/check_all_lines_matched.pl "^\s*$" "$REGEX_COMMENT_LINE" "$REGEX_DATA_LINE" < $LOGS_DIR/set_report_sort.log
+(( CHECK_EXIT_CODE += $? ))
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "checking report.sort_order variable"
+(( TEST_RESULT += $? ))
+
+
+# set back to default
+$CMD_PERF config --user report.sort_order=comm,dso,symbol
+
+
+### report.percent-limit variable
+
+PERCENTAGE=10
+
+# set the variable
+$CMD_PERF config --user report.percent-limit=$PERCENTAGE
+PERF_EXIT_CODE=$?
+
+$CMD_PERF config --user --list > $LOGS_DIR/set_report_list.log
+(( PERF_EXIT_CODE += $? ))
+
+# check if the variable is set
+grep -q report.percent-limit=$PERCENTAGE < $LOGS_DIR/set_report_list.log
+CHECK_EXIT_CODE=$?
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "setting report.percent-limit variable"
+(( TEST_RESULT += $? ))
+
+
+# check if the variable changed percentage limit
+$CMD_PERF report --stdio -i $CURRENT_TEST_DIR/perf.data > $LOGS_DIR/set_report_limit.log
+PERF_EXIT_CODE=$?
+
+CHECK_EXIT_CODE=`perl -ne 'BEGIN{$n=0;} {$n+=1 if (/\s*('$RE_NUMBER')%\s*\w+\s*/ and $1 < '$PERCENTAGE');} END{print "$n";}' < $LOGS_DIR/set_report_limit.log`
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "checking report.percent-limit variable"
+(( TEST_RESULT += $? ))
+
+
+# set back to default
+$CMD_PERF config --user report.percent-limit=0
+
+
+### report.children variable
+
+# record and report with call-graph option
+
+$CMD_PERF record -a -g -o $CURRENT_TEST_DIR/perf.data -- $CMD_SIMPLE 2> $LOGS_DIR/set_report_record_cg.log
+PERF_EXIT_CODE=$?
+
+../common/check_all_patterns_found.pl "$RE_LINE_RECORD1" "$RE_LINE_RECORD2" "perf.data" < $LOGS_DIR/set_report_record_cg.log
+CHECK_EXIT_CODE=$?
+
+$CMD_PERF report --stdio -i $CURRENT_TEST_DIR/perf.data > $LOGS_DIR/set_report_report_no_cfg_cg.log 2> /dev/null
+(( PERF_EXIT_CODE += $? ))
+
+CHILDREN_PERCENTAGE=`perl -ne 'BEGIN{$n=0;} {$n+=$1 if /^\s*('$RE_NUMBER')%\s*'$RE_NUMBER'%\s*/;} END{print "$n";}' < $LOGS_DIR/set_report_report_no_cfg_cg.log`
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "setup for report.children variable"
+(( TEST_RESULT += $? ))
+
+
+# set the variable
+$CMD_PERF config --user report.children=false
+PERF_EXIT_CODE=$?
+
+$CMD_PERF config --user --list > $LOGS_DIR/set_report_list.log
+(( PREF_EXIT_CODE += $? ))
+
+# check if the variable is set
+grep -q report.children=false < $LOGS_DIR/set_report_list.log
+CHECK_EXIT_CODE=$?
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "setting report.children variable"
+(( TEST_RESULT += $? ))
+
+
+# check if there are no children percentages
+$CMD_PERF report --stdio -i $CURRENT_TEST_DIR/perf.data > $LOGS_DIR/set_report_children.log
+PERF_EXIT_CODE=$?
+
+OVERHEAD_PERCENTAGE=`perl -ne 'BEGIN{$n=0;} {$n+=$1 if /^\s*('$RE_NUMBER')%\s*/;} END{print "$n";}' < $LOGS_DIR/set_report_children.log`
+
+CHECK_EXIT_CODE=`echo "$CHILDREN_PERCENTAGE < $OVERHEAD_PERCENTAGE" | bc`
+
+PRECISION=0.1
+ZERO=`echo $OVERHEAD_PERCENTAGE - 100 | bc | tr -d - | awk '{print $0" > '$PRECISION'"}' | bc`
+
+(( CHECK_EXIT_CODE += $ZERO ))
+
+print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "checking report.children variable"
+(( TEST_RESULT += $? ))
+
+
+# restore the config file
+mv $CURRENT_TEST_DIR/.config_before $HOME/.perfconfig
+
+
+# print overall results
+print_overall_results "$TEST_RESULT"
+exit $?
