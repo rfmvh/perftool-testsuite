3149ef918a1834f56d99928f3b960586206467c4
Benjamin Salon <bsalon@redhat.com>
2020-07-03 11:19:54 +0200

    base_config/test_set.sh: use pre prepared file to get all variables
    We do not need to compute all variables as we get them by running
    'base_config/setup.sh' file.

--- base_config/test_set.sh
+++ base_config/test_set.sh
@@ -18,23 +18,6 @@ THIS_TEST_NAME=`basename $0 .sh`
 TEST_RESULT=0
 
 
-### get all variable names
-
-$CMD_PERF config --help > $LOGS_DIR/set_help.log 2> /dev/null
-PERF_EXIT_CODE=$?
-
-sed '1,/Variables/d' < $LOGS_DIR/set_help.log > $LOGS_DIR/set_help_variables.log 2> /dev/null
-CHECK_EXIT_CODE=$?
-
-BEGIN_SPACE=`grep "colors\.\*" < $LOGS_DIR/set_help_variables.log | grep -o [[:space:]]* 2> /dev/null`
-
-grep "^$BEGIN_SPACE\w" < $LOGS_DIR/set_help_variables.log | tr -d ' ' | tr ',' '\n' | grep "\.\w" > $LOGS_DIR/set_all.log 2> /dev/null
-(( CHECK_EXIT_CODE += $? ))
-
-print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "getting all variable names"
-(( TEST_RESULT += $? ))
-
-
 # save config file before tests
 touch $HOME/.perfconfig
 mv $HOME/.perfconfig $CURRENT_TEST_DIR/.config_before
@@ -42,7 +25,7 @@ mv $HOME/.perfconfig $CURRENT_TEST_DIR/.config_before
 
 ### set all variables to default value
 
-for VAR in `cat $LOGS_DIR/set_all.log`; do
+for VAR in `cat $LOGS_DIR/config_all_variables.log 2> /dev/null`; do
 	VARSET=`grep $VAR < $CURRENT_TEST_DIR/no_default.txt 2> /dev/null`
 	if [ $? -ne 0 ]; then
 		VARSET="$VAR=default"
