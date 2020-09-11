b8c66a941fb2c3be7b7e5c7b368799e05354e81f
Benjamin Salon <bsalon@redhat.com>
2020-09-11 21:10:38 +0200

    common/check_timestamps.pl: generalize timestamps check
    The old version of this file could test only specific input, because the
    pattern we used to check timestamps was embedded in the file. This
    version takes the first argument of 'common/check_timestamps.pl' as the
    pattern, so we can now test order of any timestamps or if needed, order
    of something totally different.

--- common/check_timestamps.pl
+++ common/check_timestamps.pl
@@ -9,23 +9,24 @@ $passed = 1;
 $r = 0.0;
 $P = 0.0;
 
+$timestamp = @regexps[0];
+
 while (<STDIN>)
 {
 	s/\n//;
 
-	# skip lines with unpaired syscalls (continued ones that had been started before perf attached), such as:
-	#          ? (     ?   ): something/732  ... [continued]: poll()) = 0 Timeout
-	next if /^\s+\?\s*\(\s+\?/;
-
-	($p) = $_ =~ /^\s*(\d+(?:\.\d+))\s/;
-	$p *= 1.0;
-
-	unless ($p >= $r)
+	if (/$timestamp/)
 	{
-		print "Invalid timestamp: $p is lower than $r (previous one)\n" unless $quiet;
-		exit 1;
+		($p) = $_ =~ /$timestamp/;
+		$p *= 1.0;
+
+		unless ($p >= $r)
+		{
+			print "Invalid timestamp: $p is lower than $r (previous one)\n" unless $quiet;
+			exit 1;
+		}
+		$r = $p;
 	}
-	$r = $p;
 }
 
 
