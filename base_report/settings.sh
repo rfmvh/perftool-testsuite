rfmvh/perftool-testsuite

commit f5eec05873286b0c65b6c7edc8f46604f07e25ed

base_report: fixed available CPU detection
When booting with nosmt kernel parameter, when hyperthreads are
disabled, it seems there are only half of CPUs (from cpuinfo).
Thus, counting processors from cpuinfo leads to incorrect results.
Perf says 48, cpuinfo 24, perf is right, cpuinfo wrong; getconf
should be more reliable.

base_report/settings.sh
@@ -12,7 +12,7 @@ export MY_ARCH=`arch`
 export MY_HOSTNAME=`hostname`
 export MY_KERNEL_VERSION=`uname -r`
 export MY_CPUS_ONLINE=`nproc`
-export MY_CPUS_AVAILABLE=`cat /proc/cpuinfo | grep -P "processor\s" | wc -l`
+export MY_CPUS_AVAILABLE=`getconf -a | grep _NPROCESSORS_CONF | awk '{print $2}'`
 
 if [ -n "$PERFSUITE_RUN_DIR" ]; then
 	# when $PERFSUITE_RUN_DIR is set to something, all the logs and temp files will be placed there

commit 238a4200d223d6bddd47434c7051babb11a5a046

base_report: fixed available CPU detection again
Previous solution used to use getconf to detect the number of available
CPUs. However, it does not work correctly on PowerPC LPARs, where it
detects number of CPUs available in total on the hardware, instead of
the number of CPUs available for the given LPAR.

On a 80 CPU PowerPC box, which is divided into five LPARs per 16 CPUs,
the command shows "80" instead of correct 16.

Fixes: f5eec05873286b0c65b6c7edc8f46604f07e25ed ("base_report: fixed available CPU detection")

base_report/settings.sh
@@ -12,7 +12,7 @@ export MY_ARCH=`arch`
 export MY_HOSTNAME=`hostname`
 export MY_KERNEL_VERSION=`uname -r`
 export MY_CPUS_ONLINE=`nproc`
-export MY_CPUS_AVAILABLE=`getconf -a | grep _NPROCESSORS_CONF | awk '{print $2}'`
+export MY_CPUS_AVAILABLE=`lscpu | grep '^CPU(s)' | awk '{print $2}'`
 
 if [ -n "$PERFSUITE_RUN_DIR" ]; then
 	# when $PERFSUITE_RUN_DIR is set to something, all the logs and temp files will be placed there