f0c0bca3107915b6bbde63a3edc0e4a316a6f2c5
Benjamin Salon <bsalon@redhat.com>
2020-12-15 12:10:10 +0100

    base_script/settings.sh: function for compaction-times support
    The compaction-times script will work only if the events required by the
    script are available - we can find them in 'perf list' output. The
    function should_support_compaction_times_script returns 0 if there are
    these events, otherwise 1.

--- base_script/settings.sh
+++ base_script/settings.sh
@@ -24,32 +24,6 @@ else
 	export LOGS_DIR="."
 fi
 
-should_support_syscall_translations()
-{
-	# return values
-	# 0 = expected to support syscall translations
-	# 1 = not expected to support syscall translations
-	test -z "$PYTHON" && /usr/bin/env python -V &>/dev/null && export PYTHON="/usr/bin/env python"
-	test -z "$PYTHON" && /usr/bin/env python3 -V &>/dev/null && export PYTHON="/usr/bin/env python3"
-	test -z "$PYTHON" && /usr/bin/env python2 -V &>/dev/null && export PYTHON="/usr/bin/env python2"
-	test -z "$PYTHON" && /usr/libexec/platform-python -V &>/dev/null && export PYTHON="/usr/libexec/platform-python"
-	test -z "$PYTHON" && export PYTHON="python"
-	$PYTHON -c "import audit" 2>/dev/null
-}
-
-detect_Qt_Python_bindings()
-{
-	# return values
-	# 0 = PySide package is installed
-	# 1 = PySide package is not installed
-	test -z "$PYTHON" && /usr/bin/env python -V &>/dev/null && export PYTHON="/usr/bin/env python"
-	test -z "$PYTHON" && /usr/bin/env python3 -V &>/dev/null && export PYTHON="/usr/bin/env python3"
-	test -z "$PYTHON" && /usr/bin/env python2 -V &>/dev/null && export PYTHON="/usr/bin/env python2"
-	test -z "$PYTHON" && /usr/libexec/platform-python -V &>/dev/null && export PYTHON="/usr/libexec/platform-python"
-	test -z "$PYTHON" && export PYTHON="python"
-	$PYTHON -c "import PySide.QtSql" 2> /dev/null || $PYTHON -c "import PySide2.QtSql" 2> /dev/null
-}
-
 should_support_deltatime_option()
 {
 	# return value
@@ -64,4 +38,13 @@ should_support_reltime_option()
 	# 0 = expected to support --reltime option
 	# 1 = not expected to support --reltime option
 	perf script --help | grep -q "\-\-reltime"
+}
+
+should_support_compaction_times_script()
+{
+	# return value
+	# 0 = expected to support compaction-times script
+	# 1 = not expected to support compaction-times script
+	FIVE=`perf list | grep -P "compaction:mm_compaction_(?:begin|end|migratepages|isolate_migratepages|isolate_freepages)" | wc -l`
+	test $FIVE -ge 5
 }
