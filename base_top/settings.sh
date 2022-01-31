7cd41fe627d42625fb5886be382c37169f72a6c3
Benjamin Salon <bsalon@redhat.com>
2022-01-31 20:59:59 +0100

    base_top: creating the skeleton of perf-top command test
    Creating a new directory for perf-top command. In this commit, there are
    two new files added to the repository. The 'base_top/settings.sh' has a
    common form with function should_support_expect_script, which detects if
    the Expect command is present on the machine. This function is needed
    before the testing. The next file, 'base_top/cleanup.sh', has the same
    form as cleanup scripts in other 'base_*' directories.

---
+++ base_top/settings.sh
@@ -0,0 +1,36 @@
+#
+#	settings.sh of perf_top test
+#	Author: Michael Petlan <mpetlan@redhat.com>
+#
+#	Description:
+#		FIXME
+#
+#
+
+export TEST_NAME="perf_top"
+
+if [ -n "$PERFSUITE_RUN_DIR" ]; then
+	# when $PERFSUITE_RUN_DIR is set to something, all the logs and temp files will be placed there
+	# --> the $PERFSUITE_RUN_DIR/perf_something/examples and $PERFSUITE_RUN_DIR/perf_something/logs
+	#     dirs will be used for that
+	export PERFSUITE_RUN_DIR=`readlink -f $PERFSUITE_RUN_DIR`
+	export CURRENT_TEST_DIR="$PERFSUITE_RUN_DIR/$TEST_NAME"
+	test -d "$CURRENT_TEST_DIR" || mkdir -p "$CURRENT_TEST_DIR"
+	export LOGS_DIR="$PERFSUITE_RUN_DIR/$TEST_NAME/logs"
+	test -d "$LOGS_DIR" || mkdir -p "$LOGS_DIR"
+else
+	# when $PERFSUITE_RUN_DIR is not set, logs will be placed here
+	export CURRENT_TEST_DIR="."
+	export LOGS_DIR="."
+fi
+
+
+should_support_expect_script()
+{
+	# return values:
+	# 0 = expected to support expect script
+	# 1 = expected not to support expect script
+
+	type expect &> /dev/null
+}
+
