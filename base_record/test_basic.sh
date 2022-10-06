ed485b54ecc34c64184505ff22cdab6a9915ae14
Michael Petlan <mpetlan@redhat.com>
2022-10-07 00:04:09 +0200

    base_record/test_basic.sh: Add test for record with large -C
    In some versions, perf-record crashed with some particular large values
    given to its -C option.

--- base_record/test_basic.sh
+++ base_record/test_basic.sh
@@ -105,6 +105,18 @@ print_results $PERF_EXIT_STATUS 0 "-k mono crash"
 (( TEST_RESULT += $? ))
 
 
+### large -C number
+
+# perf used to segfault if a number too large was used with -C
+sh -c "$CMD_PERF record -C 12323431 -- true | cat" > $LOGS_DIR/basic_largeC_crash.out 2> $LOGS_DIR/basic_largeC_crash.err
+
+../common/check_no_patterns_found.pl "Segmentation fault" "SIGSEGV" "core" "dumped" "segfault" < $LOGS_DIR/basic_largeC_crash.err
+PERF_EXIT_STATUS=$?
+
+print_results $PERF_EXIT_STATUS 0 "large -C number crash"
+(( TEST_RESULT += $? ))
+
+
 # print overall results
 print_overall_results "$TEST_RESULT"
 exit $?
