4e03b433648ba04755f92c68655fac05749aabdd
Michael Petlan <mpetlan@redhat.com>
2023-02-14 17:31:15 +0100

    base_buildid/test_buildid-cache.sh: Log removed files
    It happened that only 3 of expected 4 entries were successfully removed
    within the removal test. We have logs of the cache state before and after
    removing the entries, but we don't know which is the fourth entry that
    failed to be removed. This additional log may put some light on the bug
    if it happens again.

--- base_buildid/test_buildid-cache.sh
+++ base_buildid/test_buildid-cache.sh
@@ -94,6 +94,7 @@ COUNT=`cat $LOGS_DIR/cache_list.log | wc -l`
 test $PART -gt 4 && PART=4
 
 REMOVED_FILES=`awk '{print $2}' < $LOGS_DIR/cache_list.log | ../common/pick_random.pl $PART`
+echo "$REMOVED_FILES" | tr ' ' '\n' > $LOGS_DIR/cache_files_to_be_removed.log
 
 # remove files
 PERF_EXIT_CODE=0
