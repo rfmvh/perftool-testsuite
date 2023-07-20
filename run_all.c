dbe14cc995e7d561c422245ad233c9c9eee84c68
notValord <v.m.veverka@gmail.com>
2023-07-20 16:10:08 +0200

    run_all.c: Added arguments for running selected directories common/settings.sh: Set up a env variable for cleanup
    It is possible to run tests from only selected directories
    by running run_all.c with argument -r/--run followed by a list
    of directories to run (e.g. stat, top, trace...). If list is
    left empty, all tests are run.
    The cleaunup variable was changed to an enviromental variable
    in common/settins.sh used when using argument --no-cleanup.

--- run_all.c
+++ run_all.c
@@ -15,11 +15,11 @@
 #include <string.h>
 
 #define TESTSUITE_ROOT "./"
-
+#define MAX_DIR 32
 
 /* globals */
 int verbose = 0;
-int do_cleanup = 1;
+//int do_cleanup = 1;
 int fatal_occured = 0;
 
 
@@ -110,7 +110,12 @@ int run_group(char *path)
 		perror("Cannot open inner dir.");
 
 	/* try to do clean-up */
-	if(do_cleanup)
+
+	char* clean = getenv("TESTLOG_CLEAN");
+	if (clean == NULL)				// variable doesn't exist, it was't set
+		try_shell("cleanup.sh");
+	
+	if((clean != NULL) && (strcmp(clean, "y") == 0))	// variable exists and is set to true
 		try_shell("cleanup.sh");
 
 	chdir("..");
@@ -128,6 +133,7 @@ int main(int argc, char *argv[])
 	int failures = 0;
 	int i;
 	char verbosity_str[2];
+	int select_run = 0;
 
 	for(i = 1; i < argc; i++)
 	{
@@ -147,21 +153,68 @@ int main(int argc, char *argv[])
 		/* skip the clean-up */
 		if((strcmp(argv[i], "--no-cleanup") == 0) || (strcmp(argv[i], "-n") == 0))
 		{
-			do_cleanup = 0;
+			setenv("TESTLOG_CLEAN", "n", 1);
+			//do_cleanup = 0;
 			continue;
+		}
+
+		/* run only selected directories */
+		/* to run tests only from certain directories use -r or --run followed
+			by the list of the names of directories (e.g. stat, trace, top...) to run,
+			if the list is empty, all directories are run*/
+		if((strcmp(argv[i], "-r") == 0) || (strcmp(argv[i], "--run") == 0))
+		{
+			if((i+1) < argc){
+				select_run = i+1;
+				continue;
+			}
+			fprintf(stderr, "Empty list of selected directories was provided. Running all tests.\n");
 		}
 	}
 
 	dp = opendir(TESTSUITE_ROOT);
 	if(dp != NULL)
 	{
-		while(ep = readdir(dp))
-		{
-			if(strncmp(ep->d_name, "base_", 5))
-				continue;
-			failures += run_group(ep->d_name);
+		if(select_run){
+			for(i = select_run; i < argc; i++)
+			{
+				char s_dir[MAX_DIR] = "";
+				struct stat sb;
+
+				/* check whether the argument is not different option */
+				if((strlen(argv[i])) && (argv[i][0] == '-'))
+				{
+					break;
+				}
+
+				strcat(s_dir, "base_");
+				if(strlen(argv[i]) > MAX_DIR-5){		// too long string for a directory name
+					fprintf(stderr, "Trying to run a nonexistent directory, name too long: %s\n", argv[i]);
+					continue;
+				}
+				strcat(s_dir, argv[i]);
+
+				/* check the existence of the selected directory*/
+				if(stat(s_dir, &sb) == 0 && (S_ISDIR(sb.st_mode)))
+				{
+					failures += run_group(s_dir);
+				}
+				else
+				{
+					fprintf(stderr, "Trying to run a nonexistent directory: %s\n", argv[i]);
+				}
+			}
 		}
-		closedir(dp);
+		else
+		{
+			while(ep = readdir(dp))
+			{
+				if(strncmp(ep->d_name, "base_", 5))
+					continue;
+				failures += run_group(ep->d_name);
+			}
+			closedir(dp);
+		}
 	}
 	else
 		perror("Cannot open outer dir.");
