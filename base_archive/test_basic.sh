5d2fe19344e7423b80b0b525b890bbb942fc60c3
Veronika Molnarova <vmolnaro@redhat.com>
2024-02-08 16:20:44 +0100

    base_archive/test_basic.sh: Fix regex for --unpack option
    The previous commit was not checking whether the regexes have been found
    or not. Simple fix of a mistake.

--- base_archive/test_basic.sh
+++ base_archive/test_basic.sh
@@ -99,8 +99,9 @@ REGEX_LINES_2="wherever you need to run"
 REGEX_LINES_3="perf.data.tar.bz2"
 REGEX_LINES_4="perf archive --unpack"	# after option --unpack was added
 ../common/check_all_patterns_found.pl "$REGEX_LINES_1" "$REGEX_LINES_2" "$RE_LINE_EMPTY" < $LOGS_DIR/basic_archive.log
-../common/check_any_pattern_found.pl "$REGEX_LINES_3" "$REGEX_LINES_4" < $LOGS_DIR/basic_archive.log
 CHECK_EXIT_CODE=$?
+../common/check_any_pattern_found.pl "$REGEX_LINES_3" "$REGEX_LINES_4" < $LOGS_DIR/basic_archive.log
+(( CHECK_EXIT_CODE += $? ))
 ../common/check_errors_whitelisted.pl "stderr-whitelist.txt" < $LOGS_DIR/basic_archive.err
 (( CHECK_EXIT_CODE += $? ))
 
